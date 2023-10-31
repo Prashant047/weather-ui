23d87cfa092187654c235e15ba1e92c55dc17a32
Prashant047 <prash.kumar047@gmail.com>
2023-10-31 18:18:41 +0000

    added skeleton layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,36 @@
 export default function Home() {
   return (
-    <div className="p-8">
-      <TempRangeBar
-        tenDayMin={7}
-        tenDayMax={11}
-        dayMax={11}
-        dayMin={8}
-      />
-    </div>
+    <main className="p-2">
+      <section className="md:hidden mx-auto grid max-w-sm gap-2 grid-cols-2">
+        <div className="border bg-red-500 h-[300px] col-span-2"></div>
+        <div className="border bg-blue-500 h-[500px] col-span-2"></div>
+        <div className="border bg-orange-500 h-[200px]"></div>
+        <div className="border bg-stone-500 h-[200px]"></div>
+        <div className="border bg-green-500 col-span-2 h-[200px]"></div>
+        <div className="border bg-green-500 col-span-2 h-[200px]"></div>
+        <div className="border bg-yellow-500 h-[200px]"></div>
+        <div className="border bg-orange-500 h-[200px]"></div>
+      </section>
+      <section className="hidden mx-auto md:grid gap-2 grid-cols-[1fr_1.5fr] max-w-3xl">
+        <div className="flex flex-col gap-2">
+          <div className="border bg-red-500 h-[300px]"></div>
+          <div className="border bg-blue-500 h-[500px]"></div>
+          <div className="border bg-red-500 h-[300px]"></div>
+        </div>
+        <div className="grid grid-cols-2 content-start gap-2">
+          <div className="border bg-orange-500 h-[190px]"></div>
+          <div className="border bg-stone-500 h-[190px]"></div>
+          <div className="border bg-green-500 col-span-2 h-[150px]"></div>
+          <div className="border bg-green-500 col-span-2 h-[150px]"></div>
+          <div className="border bg-yellow-500 h-[190px]"></div>
+          <div className="border bg-orange-500 h-[190px]"></div>
+          <div className="border bg-yellow-500 h-[190px]"></div>
+          <div className="border bg-yellow-500 h-[190px]"></div>
+          <div className="border bg-orange-500 h-[190px]"></div>
+          <div className="border bg-orange-500 h-[190px]"></div>
+        </div>
+      </section>
+    </main>
   );
 }
 
@@ -34,8 +57,13 @@ function TempRangeBar({
     <span className="text-neutral-500">{dayMin} <sup>&#176;</sup></span>
     <div className="flex-1 bg-neutral-800 h-1.5 relative rounded-full">
       <div 
-        className="bg-[#65a5f4] h-full absolute top-0 rounded-full"
-        style={{width: `${width}%`, left: `${left}%`}}
+        className=" h-full absolute top-0 rounded-full"
+        style={{
+          width: `${width}%`, 
+          left: `${left}%`,
+          background: `linear-gradient(90deg, rgba(100,172,243,1) 0%, rgba(132,234,175,1) 100%)`
+
+        }}
       />
     </div>
     <span className="text-neutral-400">{dayMax} <sup>&#176;</sup></span>
